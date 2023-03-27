a3f53ca8e065749178d12e355428187e1c156325
plok74122 <plok74122@gmail.com>
2023-03-27 11:53:21 +0800

    except show action in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'welcome#index'
   resources :posts do
-    resources :comments
+    resources :comments, except: :show
   end
 end
