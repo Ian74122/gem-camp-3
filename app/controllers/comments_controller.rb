589c83f3e2d92a0f510d078fd4fc3ec4bca7aa41
plok74122 <plok74122@gmail.com>
2023-03-27 11:52:18 +0800

    done new and create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,30 @@
 class CommentsController < ApplicationController
   before_action :set_post
+
   def index
     @comments = @post.comments
+  end
+
+  def new
+    @comment = @post.comments.build
+  end
+
+  def create
+    @comment = @post.comments.build(params_comment)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def params_comment
+    params.require(:comment).permit(:content)
   end
 end
