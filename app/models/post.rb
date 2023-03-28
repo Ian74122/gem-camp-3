5d3607316a6012cdee50e2c1f9bf7eea8261f189
plok74122 <plok74122@gmail.com>
2023-03-28 10:53:14 +0800

    add many to many  for post and category

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   validates :content, presence: true
 
   has_many :comments, dependent: :destroy
+  has_many :post_category_ships
+  has_many :categories, through: :post_category_ships
 end
