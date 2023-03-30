8b6f4221f19a8a765360aaaff796dc554f08a495
plok74122 <plok74122@gmail.com>
2023-03-30 10:43:50 +0800

    add paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:categories, :user, :comments).all
+    @posts = Post.includes(:categories, :user, :comments).page params[:page]
   end
 
   def new
