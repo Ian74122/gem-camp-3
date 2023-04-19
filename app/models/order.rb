c7e24afd97d74c2a1cbeb3e141cfefab2c0b5823
plok74122 <plok74122@gmail.com>
2023-04-19 10:34:31 +0800

    add state machine in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,29 @@
 class Order < ApplicationRecord
+  include AASM
   belongs_to :user
+  after_create :assign_serial_number
+  aasm column: :state do
+    state :pending, initial: true
+    state :submitted, :paid, :failed, :revoked
+
+    event :submit do
+      transitions from: :pending, to: :submitted
+    end
+
+    event :pay do
+      transitions from: :submitted, to: :paid
+    end
+
+    event :fail do
+      transitions from: [:pending, :submitted], to: :failed
+    end
+
+    event :revoke do
+      transitions from: [:pending, :submitted], to: :revoked
+    end
+  end
+
+  def assign_serial_number
+    self.update(serial_number: "gem-#{id.to_s.rjust(9, '0')}")
+  end
 end
